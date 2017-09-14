54e9c921c4d6567ea30f9d43d735b449c894ac2c
Chris Withers <chris@simplistix.co.uk>
2017-09-14 08:00:51 +0100

    refactor

--- coveralls_check.py
+++ coveralls_check.py
@@ -6,26 +6,34 @@ import sys
 url = 'https://coveralls.io/builds/{}.json'
 
 
-def message(args, data, template):
+def message(args, covered, template):
     print(template.format(
-        args.commit, data["covered_percent"], args.fail_under
+        args.commit, covered, args.fail_under
     ))
 
 
-def main():
+def get_coverage(commit):
+    response = requests.get(url.format(commit))
+    data = response.json()
+    return data['covered_percent']
+
+
+def parse_args():
     parser = ArgumentParser()
     parser.add_argument('commit', help='the commit hash to check')
     parser.add_argument('--fail-under', type=float, default=100,
                         help='Exit with a status of 2 if the total coverage is '
                              'less than MIN.')
-    args = parser.parse_args()
+    return parser.parse_args()
 
-    response = requests.get(url.format(args.commit))
-    data = response.json()
 
-    if data["covered_percent"] < args.fail_under:
-        message(args, data, 'Failed coverage check for {} as {} < {}')
+def main():
+    args = parse_args()
+    covered = get_coverage(args.commit)
+
+    if covered < args.fail_under:
+        message(args, covered, 'Failed coverage check for {} as {} < {}')
         sys.exit(2)
     else:
-        message(args, data, 'Coverage OK for {} as {} >= {}')
+        message(args, covered, 'Coverage OK for {} as {} >= {}')
 
